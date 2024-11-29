9ab5b57d4c075508ae26cc7082dc7633835a2c09
Axelou26 <axel.mancini@ynov.com>
2024-11-29 17:05:37 +0100

    v4

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -1,14 +1,11 @@
 package com.jad;
 
 public class Corge {
-    private Foo foo;
+    private Foo foo; // Référence à l'objet Foo
 
 
     public Corge(final Foo foo) {
-        this.foo = foo;
-        if (foo.getCorge() != this) {
-            foo.setCorge(this);
-        }
+        setFoo(foo);
     }
 
 
@@ -18,9 +15,26 @@ public class Corge {
 
 
     public void setFoo(Foo foo) {
+
+        if (this.foo == foo) {
+            return;
+        }
+
+
+        if (this.foo != null) {
+            this.foo.setCorge(null); // Déconnecte l'ancien Corge de Foo
+        }
+
         this.foo = foo;
+
+
+        if (foo != null && foo.getCorge() != this) {
+            foo.setCorge(this); // Établit l'association bidirectionnelle avec Foo
+        }
     }
 }
+
+
 
 
 
